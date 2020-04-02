d51c7a31ddd6d6c31c20281ccaad98852a107077
Jordan Whitten <jordanwa1947@gmail.com>
2020-04-02 10:34:58 -0600

    First test works, but the second test is still broken

--- src/NewReservationForm/NewReservationForm.test.js
+++ src/NewReservationForm/NewReservationForm.test.js
@@ -6,17 +6,20 @@ import NewReservationForm from './NewReservationForm.js';
 describe('NewReservationForm', () => {
   it('should render a new reservation form to the page', () => {
     const newReservation = jest.fn();
-    const { getByPlaceholderText } = render(<NewReservationForm />)
+    const { getByPlaceholderText, getByText } = render(<NewReservationForm />)
 
     expect(getByPlaceholderText('name')).toBeInTheDocument();
+    expect(getByText("Create Reservation")).toBeInTheDocument();
   })
 
   it('should invoke the handleClick method on submission', () => {
     const newReservation = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <NewReservationForm />)
+    const { getByTestId, getByPlaceholderText, getByText } = render(
+      <NewReservationForm addNewReservation={newReservation}/>
+    )
 
     fireEvent.change(getByPlaceholderText(/name/i), { target: { value: 'name' } })
+    fireEvent.submit(getByTestId("form"));
     fireEvent.click(getByText("Create Reservation"));
 
     expect(newReservation).toHaveBeenCalled()
