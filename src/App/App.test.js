e6927528b681fa2816878067b3f28e5d3a3d4737
Jordan Whitten <jordanwa1947@gmail.com>
2020-04-02 09:50:20 -0600

    Adds test for fetch call in the App component test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import { render, waitForElement } from '@testing-library/react'; 
+import '@testing-library/jest-dom'
+
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('It should render the text that we expect', async () => {
+    const { getByText } = render(<App />)
+
+    await waitForElement(() => getByText("Christie"))
+
+    expect(getByText("Christie")).toBeInTheDocument();
+    expect(getByText("12/29")).toBeInTheDocument();
+    expect(getByText("Leta")).toBeInTheDocument();
+    expect(getByText("4/5")).toBeInTheDocument();
+  })
+})
